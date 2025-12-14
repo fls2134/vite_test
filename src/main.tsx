db946661bb2b4c04545f1ddd6f4f90a4e20b8d45
fls2134 <fls213444@gmail.com>
2025-12-14 21:09:02 +0900

    Fix: Add basename to BrowserRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/vite_test">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
